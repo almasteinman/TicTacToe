8369fd4bbf7d9ace4e3fd5f49da52466aef6e305
almasteinman <alma.steinman@gmail.com>
2023-12-08 12:12:06 +0200

    create isWinner function

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     String turn;
@@ -14,12 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onNewGame();
     }
 
     private void onNewGame() {
         board = new String[3][3];
-        for (int row=0; row < 3; row++)
-            for (int col=0; col < 3; col++)
+        for (int row = 0; row < 3; row++)
+            for (int col = 0; col < 3; col++)
                 board[row][col] = new String();
 
         turn = "X";
@@ -27,7 +31,85 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @SuppressLint("NonConstantResourceId")
     public void onButtonClick(View view) {
+        if (view.getId() == R.id.btn_00)
+            handleClick(0, 0, R.id.btn_00);
+        if (view.getId() == R.id.btn_01)
+            handleClick(0, 1, R.id.btn_01);
+        if (view.getId() == R.id.btn_02)
+            handleClick(0, 2, R.id.btn_02);
+        if (view.getId() == R.id.btn_10)
+            handleClick(1, 0, R.id.btn_10);
+        if (view.getId() == R.id.btn_11)
+            handleClick(1, 1, R.id.btn_11);
+        if (view.getId() == R.id.btn_12)
+            handleClick(0, 2, R.id.btn_12);
+        if (view.getId() == R.id.btn_20)
+            handleClick(2, 0, R.id.btn_20);
+        if (view.getId() == R.id.btn_21)
+            handleClick(2, 1, R.id.btn_21);
+        if (view.getId() == R.id.btn_22)
+            handleClick(2, 2, R.id.btn_22);
+
+
 
     }
+
+    private void handleClick(int row, int col, int id) {
+        if (board[row][col].equals("")) {
+            board[row][col] = turn;
+            Button btn = findViewById(id);
+            btn.setText(turn);
+            onTurnEnd();
+        }
+    }
+
+    private void onTurnEnd() {
+        if (isWinner())
+            endGame(turn + " won!");
+        else {
+            count++;
+            if (count == 9)
+                endGame("Tie");
+            else {
+                turn = (turn.equals("X") ? "O" : "X");
+            }
+        }
+    }
+
+    private void endGame(String s) {
+
+        //show winner
+    }
+
+    private boolean isWinner() {
+        //win row
+        for (int row = 0; row < 3; row++) {
+            if (board[row][0].toString() == board[row][1].toString())
+                if (board[row][0].toString() == board[row][2].toString())
+                    return true;
+        }
+        //win col
+        for (int col = 0; col < 3; col++) {
+            if (board[0][col].toString() == board[1][col].toString())
+                if (board[0][col].toString() == board[2][col].toString())
+                    return true;
+        }
+        //win diagonal left up to right down
+        if (board[0][0].toString() == board[1][1].toString()) {
+            if (board[0][0].toString() == board[2][2].toString())
+                return true;
+        }
+        //win diagonal right up to left down
+        if (board[2][0].toString() == board[1][1].toString()) {
+            if (board[2][0].toString() == board[0][2].toString())
+                return true;
+        }
+        return false;
+    }
 }
+
+
+
+
